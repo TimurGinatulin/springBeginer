e5951c7c2abdf7fd980e7103b2459a3ce409b959
Tim <ginatulin.t@yandex.ru>
2021-06-20 22:52:23 +0500

    hw

---
+++ springMod/src/main/java/home/ginatulin/services/ClientRepositoryConnector.java
@@ -0,0 +1,48 @@
+package home.ginatulin.services;
+
+import home.ginatulin.models.Product;
+import home.ginatulin.repositories.ProductRepositoryImp;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+class ClientRepositoryConnector {
+    @Autowired
+    private ProductRepositoryImp repositoryImp;
+
+    protected List<Product> getAllProductList() {
+        return repositoryImp.getAllProductList();
+    }
+
+    protected Product getProductById(int id) {
+        return repositoryImp.getProductById(id);
+    }
+
+    protected Product getProductByTitle(String title) {
+        return repositoryImp.getProductByTitle(title);
+    }
+
+    protected int getCountProduct(String title) {
+        return repositoryImp.getCountProduct(title);
+    }
+
+    protected int getCountProduct(int id) {
+        return repositoryImp.getCountProduct(id);
+    }
+
+    protected boolean decrementingCountProduct(int id, int count) {
+        return repositoryImp.decrementingCountProduct(id, count);
+    }
+
+    protected boolean decrementingCountProduct(String title, int count) {
+        return repositoryImp.decrementingCountProduct(title, count);
+    }
+
+    protected boolean incrementingCountProduct(int id, int count) {
+        return repositoryImp.incrementingCountProduct(id, count);
+    }
+
+    protected boolean incrementingCountProduct(String title, int count) {
+        return repositoryImp.incrementingCountProduct(title, count);
+    }
+}
